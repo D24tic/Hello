0e4f024193990bb474be479c8c577631c02b0630
D24tic <90190981+D24tic@users.noreply.github.com>
2021-09-13 13:12:23 +0200

    🎉 första commit

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
 print("hejsan")
+
+tal1 = 12
+tal2 = float(input("skriv ett tal: "))
+
+summa = tal1 + tal2
+print(f"Summan blir {summa}")
+summa = tal1 * tal2
+print(f"Summan blir {summa}")
+summa = tal1 - tal2
+print(f"Summan blir {summa}")
+summa = tal1 / tal2
+
+print(f"Summan blir {summa}")
